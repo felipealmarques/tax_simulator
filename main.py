86d3dc2b4a746c32feb6ee371a9e2f046a0b0052
felipealmarques <felipealmarques02@gmail.com>
2026-02-22 12:56:34 -0300

    feat: update eninge.py for tts comparation

--- main.py
+++ main.py
@@ -7,25 +7,55 @@ from tax_simulator.data_ingestion.repositories import ICMSRepository
 
 from tax_simulator.config.config_schema import TaxSimulatorCfg
 from tax_simulator.config.config_store import register
+from tax_simulator.utils.helpers import print_results
 
 register()
 
+# Função principal
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def main(cfg: TaxSimulatorCfg):
 
     repo = ICMSRepository(cfg)
     engine = ICMSPolicyEngine(repo)
 
-    operacao = Operation(
-        uf_origin="SP",
-        uf_destiny="MG",
+    # configurações
+    uf_origin = "SP"
+    uf_destiny = "BA"
+    monthly_amount = 10_000
+    months = 12
+
+    origin_operation = Operation(
+        uf_origin=uf_origin,
+        uf_destiny=uf_destiny,
         operation_date=date.today(),
         merchandise_origin=OriginMerchandise.NATIONAL
     )
 
-    aliquot = engine.aliquot_calculate(operacao)
+    extrema_operation = Operation(
+        uf_origin="MG",
+        uf_destiny=uf_origin,
+        operation_date=date.today(),
+        merchandise_origin=OriginMerchandise.NATIONAL
+    )
 
+    # Calculo da alíquota
+    aliquot = engine.aliquot_calculate(origin_operation)
     print(f"Alíquota: {aliquot}%")
+
+    # Comparação dos cenários
+    scenario_comparation = engine.scenario_comparation(
+        origin_operation=origin_operation,
+        extrema_operation=extrema_operation,
+        monthly_amount=monthly_amount,
+        months=months
+    )
+
+    print_results(
+        scenario_comparation=scenario_comparation, 
+        uf_origin=uf_origin,
+        monthly_amount=monthly_amount,
+        months=months
+    )
 
 
 if __name__ == "__main__":
---
+++ src/tax_simulator/utils/helpers.py
@@ -0,0 +1,38 @@
+from tax_simulator.domain.models import ScenarioComparation
+
+def print_results(
+        scenario_comparation: ScenarioComparation, 
+        uf_origin: str,
+        monthly_amount: float,
+        months: int
+) -> None:
+    result = f"""
+    #####################################################
+    ###             Comparação dos Resultados         ###
+    #####################################################
+    -------- Configurações da Operação --------
+    Valor mensal: R${round(monthly_amount, 2):,}
+    Quantidade de meses: {months}
+    Valor total (sem alíquota): R${round(monthly_amount * months, 2):,}
+    
+    -------- Cenário Normal ({uf_origin}) --------
+    Alíquota Interestadual: {scenario_comparation.normal_scenario.interstate}%
+    DIFAL: {scenario_comparation.normal_scenario.difal}%
+    Alíquota total: {scenario_comparation.normal_scenario.total_aliquot}%
+    Valor ICMS total: R${round(scenario_comparation.normal_scenario.icms_value, 2):,}
+    Valor DIFAL: R${round(scenario_comparation.normal_scenario.difal_value, 2):,}
+    Valor total: R${round(scenario_comparation.normal_scenario.total_value, 2):,}
+
+    -------- Cenário Extrema (MG) --------
+    Alíquota Interestadual: {scenario_comparation.tts_scenario.interstate}%
+    DIFAL: {scenario_comparation.tts_scenario.difal}%
+    Alíquota total: {scenario_comparation.tts_scenario.total_aliquot}%
+    Valor ICMS total: R${round(scenario_comparation.tts_scenario.icms_value, 2):,}
+    Valor DIFAL: R${round(scenario_comparation.tts_scenario.difal_value, 2):,}
+    Valor total: R${round(scenario_comparation.tts_scenario.total_value, 2):,}
+
+    -------- Economia Geral --------
+        R${round(scenario_comparation.total_savings, 2)}
+    """.replace(".","^").replace(",",".").replace("^",",")
+
+    print(result)
